2bc28227d39e256e27b88ca667c1698ff56fc38a
Malik Shalash <mjshalash@rci4u.com>
2025-10-09 00:28:32 -0400

    Filtered winner column
    WIP: Need to make query more efficient
    WIP: Need to programatically isolate all the times DEN, CAR or DAL won in the same week and year

--- seasons.py
+++ seasons.py
@@ -1,6 +1,9 @@
 import nflreadpy as nfl
 import polars as pl
 
+# Set the maximum number of rows to display to 50
+pl.Config.set_tbl_rows(1000)
+
 schedule_stats = nfl.load_schedules()
 
 filtered_columns = schedule_stats.filter(
@@ -8,6 +11,8 @@ filtered_columns = schedule_stats.filter(
             (pl.col("away_team").str.contains("DEN|CAR|DAL")) | (pl.col("home_team").str.contains("DEN|CAR|DAL"))
         ).select(
             ["season","week","result","away_team","home_team"]
-        ).with_columns(pl.when(pl.col("result") > 0).then("home_team").otherwise("away_team").alias("Winner"))
+        ).with_columns(
+            pl.when(pl.col("result") > 0).then("home_team").otherwise("away_team").alias("winner")
+        ).filter(pl.col("winner").str.contains("DEN|CAR|DAL"))
 
 print(filtered_columns)
